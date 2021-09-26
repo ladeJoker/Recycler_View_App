9c23ea2677990bedf456abc0cadf7fbc229b786d
Ghader Najjar <ghadernagar@gmail.com>
2021-09-26 19:32:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.recyclerviewapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    lateinit var addButton: Button
+    lateinit var userEntry: EditText
+    lateinit var tasks: ArrayList<String>
+    lateinit var myRV: RecyclerView
+    lateinit var appAlert: ConstraintLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        addButton = findViewById(R.id.btAdd)
+        userEntry = findViewById(R.id.etTask)
+        tasks = arrayListOf()
+        myRV = findViewById(R.id.rvMain)
+
+        appAlert = findViewById(R.id.taskAlert)
+        addButton.setOnClickListener (){ addTask() }
+
+    }
+
+    fun addTask() {
+        var check = true
+        var input = userEntry.text
+        try {
+            Integer.parseInt(input.toString())
+        }catch (e:Exception){
+            check = false
+        }
+
+        if(check == true){
+            Snackbar.make(appAlert , "Enter a Task!!", Snackbar.LENGTH_LONG).show()
+        }else {
+            tasks.add(input.toString())
+            myRV.adapter = RecyclerViewAdapter(tasks)
+            myRV.layoutManager = LinearLayoutManager(this)
+            userEntry.setText("")
+        }
+    }
+}
